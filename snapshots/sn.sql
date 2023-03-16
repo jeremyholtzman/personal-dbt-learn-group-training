24d19582ed303f7d7e2921bf54c7ca12f029dd3e
Jeremy Holtzman <jeremy.holtzman@fishtownanalytics.com>
2023-03-16 14:26:31 +0000

    fix

--- snapshots/sn.sql
+++ snapshots/sn.sql
@@ -5,7 +5,7 @@
           target_schema=generate_schema_name('snapshots'),
           strategy='timestamp',
           unique_key='id',
-          updated_at='updated_at',
+          updated_at='_etl_loaded_at',
           invalidate_hard_deletes=True,
         )
     }}
